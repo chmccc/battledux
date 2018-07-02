1dac7e9fc21eed022504ebd9efa0045f584215de
Colin McCarthy <colin@Colins-MacBook-Pro.local>
2018-07-02 15:49:28 -0700

    setup redux

---
+++ src/reducer/randomBoard.js
@@ -0,0 +1,7 @@
+const createHitMissBoard = (n = 8) => new Array(n).fill(new Array(n).fill('W'));
+const createDucksBoard = (n = 8, numGeese = 1, numDucks = 1, numDucklings = 1)
+
+const board = {
+  hits: createHitMissBoard(),
+  
+}
