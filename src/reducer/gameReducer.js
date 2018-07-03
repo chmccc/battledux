c52b62718d5fc73d97a021f160d3e093985665d7
James Tu <james@2-bit-toys.com>
2018-07-03 14:13:27 -0700

    handles cell clicks, need to check hit/miss next

--- src/reducer/gameReducer.js
+++ src/reducer/gameReducer.js
@@ -1,10 +1,11 @@
 import * as actionType from '../actions/actionTypes';
 import createBoard from './randomBoard';
+// import { Object } from 'core-js';
 
 const playerBoard = createBoard();
 const compBoard = createBoard();
 
-console.log('playerboard: ', playerBoard);
+// console.log('playerboard: ', playerBoard);
 
 const initialState = {
   playerBoard,
@@ -12,13 +13,30 @@ const initialState = {
 }
 
 const gameReducer = (state = initialState, action) => {
+  console.log("action.type: ", action.type);
   switch (action.type) {
     case actionType.PLAYER_FIRE:
-      console.log('state: ', state);
-      return state + action.payload;
+      console.log('action.payload: ', action.payload);
+
+      // check to see if we hit any of the opponent's ships
+
+      
+      return Object.assign({}, state);
+
+      case actionType.COMP_FIRE:      
+      console.log('action.payload: ', action.payload);
+      return Object.assign({}, state);
+
     default:
       return state;
   }
 }
+
+// this will update the board
+const checkHit = (col, row, board) => {
+  
+  return
+}
+
 
 export default gameReducer;
