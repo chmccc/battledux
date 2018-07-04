aafe3a7f41dc80a820e465ae64d5cf5c13364f43
Stacy Adelsberger <sladelsberger@gmail.com>
2018-07-03 21:46:35 -0700

    added login redux thunk action that connects the client to the server

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,2 +1,3 @@
 export const PLAYER_FIRE = 'PLAYER_FIRE';
 export const COMP_FIRE = 'COMP_FIRE';
+export const LOAD_BOARD = 'LOAD_BOARD';
--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,6 +1,5 @@
 import * as actionTypes from './actionTypes';
 
-
 export function playerFire(coord) {
   return {
     type: actionTypes.PLAYER_FIRE,
@@ -16,4 +15,31 @@ export function compFire() {
   }
 }
 
+export function login(data){
+  console.log("what is data?", data);
+  return dispatch => {
+    var configuration = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({username: data}),
+    }
+    return fetch('http://localhost:5000/login', configuration)
+      .then((res) => {
+        res.json()
+          .then((res2) => {
+            console.log(res2);
+            dispatch(loadBoard(res2))
+          })
+      })
+  }
+}
 
+
+export function loadBoard(datafromDB) {
+  return{
+    type: actionTypes.LOAD_BOARD,
+    payload: datafromDB
+  }
+}
