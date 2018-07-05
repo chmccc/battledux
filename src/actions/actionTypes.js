c4be9a1069d93b99b620d514fc760b807d43fa3f
Colin McCarthy <colin@Colins-MacBook-Pro.local>
2018-07-05 16:55:13 -0700

    update new game and load game reducer actions

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,3 +1,5 @@
 export const PLAYER_FIRE = 'PLAYER_FIRE';
 export const COMP_FIRE = 'COMP_FIRE';
-export const LOAD_BOARD = 'LOAD_BOARD';
+export const LOAD_GAME = 'LOAD_GAME';
+export const NEW_GAME = 'NEW_GAME';
+export const SAVE_GAME = 'SAVE_GAME';
