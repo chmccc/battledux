997427ee56d46ff311bb8554aba1c98a49403f41
Stacy Adelsberger <sladelsberger@gmail.com>
2018-07-05 14:55:27 -0700

    cleaned up routes

--- server.js
+++ server.js
@@ -17,33 +17,9 @@ app.post('/create', controller.createGame);
 
 app.get('/load/:id', controller.loadGame);
 
-app.post('/update', controller.showGames);
+app.post('/update', controller.updateGame);
 
 app.post('/login', controller.login);
 
-app.post('/', function(req, res, next) {
-    // pg.connect(connectionString, function(err, client, done) {
-    //     if (err) {
-    //         console.log("not able to get connection " + err);
-    //         res.status(400).send(err);
-    //     }
-    //     client.query(query, function(err, result) {
-    //         done();
-    //         if(err) {
-    //             console.log(err);
-    //             res.status(400).send(err);
-    //         }
-    //         res.status(200);
-    //     });
-    //     client.query('SELECT * FROM "user"', function(err, result) {
-    //         done();
-    //         if(err) {
-    //             console.log(err);
-    //             res.status(400).send(err);
-    //         }
-    //         res.status(200).send(result.rows);
-    //     });
-    // });
-});
 
 app.listen(5000, () => console.log('Listening on port 5000!'));
